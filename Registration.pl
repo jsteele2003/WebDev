0e6856353a62fb44e1fa68064767ba4da2ee0e6e
kahojon <jonathan.leung@mail.mcgill.ca>
2015-04-09 21:05:38 -0400

    Update Registration.pl

--- Registration.pl
+++ Registration.pl
@@ -25,15 +25,15 @@ for(my $i=0; $i<=$#lines; $i++){
 	}
 }
 close(LOG);
-if($existing == 0){
-	open (LOG, ">>members.csv") || Error('open','file');
-	print LOG "$name $username $password\n";
-	close(LOG);
-	print qq(<html><head><title>Congratulations</title></head>);
-	print qq(<body><p>Your registration is successful!</p><nav><a href="http://cgi.cs.mcgill.ca/~tweins2/welcome.html">Go Back To Welcome Page</a></nav></body></html>);
-}
-else{
-	print "Error: Username already exists";
-	print "Location: http://cgi.cs.mcgill.ca/~tweins2/member.html";
-}
+#if($existing == 0){
+#	open (LOG, ">>members.csv") || Error('open','file');
+#	print LOG "$name $username $password\n";
+#	close(LOG);
+#	print qq(<html><head><title>Congratulations</title></head>);
+#	print qq(<body><p>Your registration is successful!</p><nav><a href="http://cgi.cs.mcgill.ca/~tweins2/welcome.html">Go Back To Welcome Page</a></nav></body></html>);
+#}
+#else{
+#	print "Error: Username already exists";
+#	print "Location: http://cgi.cs.mcgill.ca/~tweins2/member.html";
+#}
 exit 0;
